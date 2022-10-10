6be10715e1d7a18380fe79645618e95e98d13234
dilan.dashintha <dilan.dashintha@pickme.lk>
2022-10-10 11:00:53 +0530

    changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,17 +5,6 @@ import { environment} from '../environments/environment';
 export class AppConfig {
 
   static API_PATH = environment.apiPath;
-  static RESPONCE_CODE = {
-    OK: 200,
-    CREATED: 201,
-    NO_CONTENT: 204,
-    PAGE_NOT_FOUND: 404,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    UNPROCESSABLE: 422,
-    SERVER_ERROR: 500,
-    BAD_GATEWAY: 502
-  };
 
 
 }
